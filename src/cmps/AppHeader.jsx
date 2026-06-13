c9dab440a1f4b11669f510d52ceb52757bda01d1
Rinat <ladyubaitours@gmail.com>
2026-06-13 21:24:42 +0400

    Minor design changes

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,8 +1,18 @@
+// export function AppHeader() {
+//   return (
+//     <header className="app-header">
+//       <section className="header-container">
+//         <h1>Airbnb</h1>
+//       </section>
+//     </header>
+//   )
+// }
+
 export function AppHeader() {
   return (
     <header className="app-header">
       <section className="header-container">
-        <h1>Airbnb</h1>
+        <h1 className="logo">airbnb</h1>
       </section>
     </header>
   )
