185ec8f7dee94c24074f8548b8f473f3bc683552
Rinat <ladyubaitours@gmail.com>
2026-06-18 16:31:27 +0400

    Design and improve login and login registration

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,52 +1,4 @@
-// import { useState } from 'react'
-// import { useNavigate } from 'react-router-dom'
-// import { userService } from '../services/user.service.js'
 
-// export function LoginPage({ onSetUser }) {
-//     const [credentials, setCredentials] = useState({ username: '', password: '' })
-//     const navigate = useNavigate()
-
-//     function handleChange(ev) {
-//         const { name, value } = ev.target
-//         setCredentials({ ...credentials, [name]: value })
-//     }
-
-//     async function onLogin(ev) {
-//         ev.preventDefault()
-//         try {
-//             const user = await userService.login(credentials.username, credentials.password)
-//             onSetUser(user)
-//             navigate('/')
-//         } catch (err) {
-//             alert('Login failed: ' + err)
-//         }
-//     }
-
-//     return (
-//         <section className="login-page">
-//             <h2>Login</h2>
-//             <form onSubmit={onLogin}>
-//                 <input
-//                     type="text"
-//                     name="username"
-//                     placeholder="Username"
-//                     value={credentials.username}
-//                     onChange={handleChange}
-//                     required
-//                 />
-//                 <input
-//                     type="password"
-//                     name="password"
-//                     placeholder="Password"
-//                     value={credentials.password}
-//                     onChange={handleChange}
-//                     required
-//                 />
-//                 <button>Login</button>
-//             </form>
-//         </section>
-//     )
-// }
 
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -55,6 +7,7 @@ import { login } from '../store/actions/user.actions.js'
 
 export function LoginPage() {
     const [credentials, setCredentials] = useState({ username: '', password: '' })
+    const [error, setError] = useState(false)
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
@@ -69,32 +22,58 @@ export function LoginPage() {
             await dispatch(login(credentials))
             navigate('/')
         } catch (err) {
-            alert('Login failed: ' + err)
+         setError(true)
         }
     }
-
-    return (
-        <section className="login-page">
-            <h2>Login</h2>
-            <form onSubmit={onLogin}>
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    value={credentials.username}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={credentials.password}
-                    onChange={handleChange}
-                    required
-                />
-                <button>Login</button>
-            </form>
-        </section>
-    )
+return (
+    <section className="login-page">
+       
+        <form onSubmit={onLogin}>
+            <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={credentials.username}
+                onChange={handleChange}
+                className={error ? 'input-error' : ''}
+                required
+            />
+            <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={credentials.password}
+                onChange={handleChange}
+                className={error ? 'input-error' : ''}
+                required
+            />
+            {error && <p className="error-msg">Username or password is incorrect</p>}
+            <button>Login</button>
+        </form>
+    </section>
+)
+    // return (
+    //     <section className="login-page">
+           
+    //         <form onSubmit={onLogin}>
+    //             <input
+    //                 type="text"
+    //                 name="username"
+    //                 placeholder="Username"
+    //                 value={credentials.username}
+    //                 onChange={handleChange}
+    //                 required
+    //             />
+    //             <input
+    //                 type="password"
+    //                 name="password"
+    //                 placeholder="Password"
+    //                 value={credentials.password}
+    //                 onChange={handleChange}
+    //                 required
+    //             />
+    //             <button>Login</button>
+    //         </form>
+    //     </section>
+    // )
 }
