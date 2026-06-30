f04541ca0d36f6e2ed75213a1897cbfd2b2c78d0
Amirvil <amir.vilensky@gmail.com>
2026-06-30 13:28:08 +0300

    Design SignupPage

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { signup } from '../store/actions/user.actions.js'
+import airbnb from '../assets/icons/airbnb.svg'
 
 export function SignupPage() {
     const [credentials, setCredentials] = useState({ fullname: '', username: '', password: '' })
@@ -26,34 +27,46 @@ export function SignupPage() {
 
     return (
         <section className="signup-page">
-            
-            <form onSubmit={onSignup}>
-                <input
-                    type="text"
-                    name="fullname"
-                    placeholder="Full name"
-                    value={credentials.fullname}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    value={credentials.username}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={credentials.password}
-                    onChange={handleChange}
-                    required
-                />
-                <button>Sign Up</button>
-            </form>
+
+            <div className="signup-card">
+
+                <div className="signup-header">
+                    <img className="logo" src={airbnb} alt="logo" />
+                    <h1>Create Your Account</h1>
+                    <p>It's free and always will be.</p>
+                </div>
+
+                <form onSubmit={onSignup}>
+                    <input
+                        type="text"
+                        name="fullname"
+                        placeholder="Full name"
+                        value={credentials.fullname}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        value={credentials.username}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={credentials.password}
+                        onChange={handleChange}
+                        required
+                    />
+                    <button>Sign Up</button>
+                </form>
+                <p className="login-p">Already have an account? <span className="login-link" onClick={() => navigate('/login')}>Log in</span></p>
+            </div>
+
+
         </section>
     )
 }
